5ea92c9758ffb992d5fd060095e82b15144ec4d8
est_calexx <10johannesmunoz@gmail.com>
2025-12-07 10:53:55 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HabitTracker());
+
+class HabitTracker extends StatelessWidget {
+  const HabitTracker({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        useMaterial3: false,
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF0A0E21)
+        )
+      ),
+
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Title"),
+      ),
+    );
+  }
+}
