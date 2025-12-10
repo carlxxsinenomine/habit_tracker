c250bc376ac71843f97aecf9d440f0a5f76f7f3e
est_calexx <10johannesmunoz@gmail.com>
2025-12-10 12:07:11 +0800

    ADDED: header section and some spacing and design

--- lib/pages/input_page.dart
+++ lib/pages/input_page.dart
@@ -11,40 +11,76 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: <Widget>[
-          // Text and Notifications
-          Expanded(
-            child: Container(
-
-            ),
-          ),
-          // Day section
-          Expanded(
-            child: Container(
-                margin: EdgeInsets.only(left: 10.0),
-                child: Row(
-                children: <Widget>[
-                  DayBox(color: Color(0xFF8BAE66))
-                ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.symmetric(horizontal: 17.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 53.0,
+              ),
+              // Text and Notifications
+              // Header
+              Expanded(
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                  'Good Morning,',
+                                  style: TextStyle(
+                                      fontFamily: 'Obviously',
+                                      fontSize: 25.0,
+                                      color: Colors.black,
+                                  )
+                              ),
+                              Text(
+                                  'Calexx',
+                                  style: TextStyle(
+                                    fontFamily: 'Obviously',
+                                    fontSize: 40.0,
+                                    color: Colors.black,
+                                    height: 0.5,
+                                  )
+                              ),
+                            ],
+                          )
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              // Day section
+              Expanded(
+                child: Container(
+                    child: Row(
+                    children: <Widget>[
+                      DayBox(color: Color(0xFF8BAE66))
+                    ],
+                  )
+                ),
+              ),
+              // Habits section
+              Expanded(
+                child: Container(
+                  child: Text('Habits'),
+                ),
+              ),
+              // Buttons section
+              Expanded(
+                child: Container(
+                  child: Text('Buttons'),
+                ),
               )
-            ),
+            ],
           ),
-          // Habits section
-          Expanded(
-            child: Container(
-              child: Text('Habits'),
-            ),
-          ),
-          // Buttons section
-          Expanded(
-            child: Container(
-              child: Text('Buttons'),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
