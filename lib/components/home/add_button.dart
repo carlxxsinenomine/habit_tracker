c6688025427d701e530e00597a928dc485c61d55
est_calexx <10johannesmunoz@gmail.com>
2025-12-20 18:26:10 +0800

    ADDED: Popup feature

--- lib/components/home/add_button.dart
+++ lib/components/home/add_button.dart
@@ -6,73 +6,62 @@ import 'package:habit_tracker/constants.dart';
 import 'custom_rect_tween.dart';
 import 'hero_dialog_route.dart';
 
-class AddButton extends StatefulWidget {
+class AddButton extends StatelessWidget {
   final VoidCallback onPressed;
 
+  static const double addAngle = 0.785398;
+  static const double exitAngle = 1.57079632679;
+
+  static double angle = addAngle;
   const AddButton({
     super.key,
     required this.onPressed
   });
 
   @override
-  State<AddButton> createState() => _AddButtonState();
-}
-
-class _AddButtonState extends State<AddButton> {
-  static const double addAngle = 0.785398;
-  static const double exitAngle = 1.57079632679;
-
-  bool isPressed = false;
-
-  @override
   Widget build(BuildContext context) {
+
     return GestureDetector(
-      child: AnimatedRotation(
-        turns: isPressed ? (exitAngle / (2 * pi)) : (addAngle / (2 * pi)),
-        duration: Duration(milliseconds: 100),
-        child: Container(
-          width: 80,
-          height: 80,
-          decoration: BoxDecoration(
-              color: kAddButtonColor,
-              borderRadius: BorderRadius.circular(15.0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  offset: Offset(6, 4),
-                )
-              ]
-          ),
-          child: Transform.rotate(
-            angle: -0.785398,
-            child: Icon(
-              Icons.add,
-              color: Colors.black54,
-              size: 45,
+      onTap: () {
+        Navigator.of(context).push(HeroDialogRoute(builder: (context) {
+          return const _AddTodoPopupCard();
+        }));
+      },
+      child: Hero(
+        tag: _heroAddHabit,
+        createRectTween: (begin, end) {
+          return CustomRectTween(begin: begin!, end: end!);
+        },
+        child: AnimatedRotation(
+          turns: (addAngle / (2 * pi)), // sabi sa docs need daw 2*pi eh AHAHAHA
+          duration: Duration(milliseconds: 100),
+          child: Container(
+            width: 80,
+            height: 80,
+            decoration: BoxDecoration(
+                color: kAddButtonColor,
+                borderRadius: BorderRadius.circular(15.0),
+                boxShadow: <BoxShadow>[
+                  BoxShadow(
+                    offset: Offset(6, 4),
+                  )
+                ]
+            ),
+            child: Transform.rotate(
+              angle: -0.785398,
+              child: Icon(
+                Icons.add,
+                color: Colors.black54,
+                size: 45,
+              ),
             ),
           ),
         ),
       ),
-      onTap: () {
-        setState(() {
-          isPressed = true;
-        });
-
-        Future.delayed(Duration(milliseconds: 50), () {
-          Navigator.of(context).push(HeroDialogRoute(builder: (context) {
-            return _AddTodoPopupCard();
-          })).then((_) {
-            // Reset when dialog is closed
-            if (mounted) {
-              setState(() {
-                isPressed = false;
-              });
-            }
-          });
-        });
-      },
     );
   }
 }
+
 
 // https://github.com/funwithflutter/flutter_ui_tips/blob/master/tip_003_popup_card/lib/add_todo_button.dart
 const String _heroAddHabit = 'add-habit-hero';
