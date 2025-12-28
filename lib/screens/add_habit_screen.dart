3beaee076c064dcd40163c08030b31422bcba608
est_calexx <10johannesmunoz@gmail.com>
2025-12-28 18:37:36 +0800

    ADDED: Time Picker

--- lib/screens/add_habit_screen.dart
+++ lib/screens/add_habit_screen.dart
@@ -13,7 +13,39 @@ class _AddHabitScreenState extends State<AddHabitScreen> {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          Text(
+              "Habit Name",
+              style: TextStyle(
+                color: Colors.black,
+              )
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Enter Habit Name',
+              hintStyle: TextStyle(
+                color: Colors.black,
+              )
+            ),
+          ),
+          Text(
+              "Repeat",
+              style: TextStyle(
+                color: Colors.black,
+              )
+          ),
+          ElevatedButton(
+              onPressed: () async {
+                final TimeOfDay? time = await showTimePicker(
+                    context: context,
+                    initialTime: TimeOfDay.now(),
+                );
+              },
+              child: Text("Select Time")
+          ),
 
         ],
       ),
